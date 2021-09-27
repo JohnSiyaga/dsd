361eca8fc1877ad1b2e44128d4d09b2648fd10e3
John <johnsiyaga@gmail.com>
2021-09-26 20:22:25 -0400

    Added barrel shifter example for Assignment 1

---
+++ assignment1/barrel_shifter.vhdl
@@ -0,0 +1,36 @@
+-- https://github.com/lorsi96/VHDL-Examples/blob/master/barrel_shifter
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity barrel_shifter is
+    generic(N: natural:= 4);
+    port(
+        a: in std_logic_vector(N**2-1 downto 0);
+        des: in std_logic_vector(N-1 downto 0);
+        s: out std_logic_vector(N**2-1 downto 0)
+    );
+end;
+
+architecture barrel_shifter_arch of barrel_shifter is
+
+    function word_to_int(word: std_logic_vector) return integer is 
+        variable result : integer := 0; 
+    begin 
+        for index in word'range loop 
+            result := result * 2; 
+            if (word(index) = '1') then 
+                    result := result + 1; 
+            end if; 
+        end loop; 
+            return result; 
+    end word_to_int; 
+
+    begin
+        process(a)
+            variable int_temp: integer;
+        begin
+            int_temp := word_to_int(des);
+            s <=  a(N**2 - 1 - int_temp downto 0) & a(N**2-1 downto N**2 - int_temp);
+        end process;
+    end;
---
+++ assignment1/barrel_shifter_tb.vhdl
@@ -0,0 +1,45 @@
+-- https://github.com/lorsi96/VHDL-Examples/blob/master/barrel_shifter
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+
+entity barrel_shifter_tb is
+end;
+
+architecture barrel_shifter_tb_arch of barrel_shifter_tb is
+    -- Component Declaration --
+    component barrel_shifter is
+        generic(N: natural:=2);
+        port(
+            a: in std_logic_vector(2**N-1 downto 0);
+            des: in std_logic_vector(N-1 downto 0);
+            s: out std_logic_vector(2**N-1 downto 0)
+        );
+    end component;
+
+    signal a_tb: std_logic_vector(3 downto 0);
+    signal des_tb: std_logic_vector(1 downto 0);
+    signal s_tb: std_logic_vector(3 downto 0);
+    
+begin
+
+    -- Sequence Goes Here --
+
+    DUT: barrel_shifter
+        port map(
+            a => a_tb,
+            des => des_tb,
+            s => s_tb
+        );
+    
+    TEST: process
+    begin
+        a_tb <= "0001";
+        des_tb <= "11";
+        wait for 20 ns;
+        a_tb <= "0011";
+        wait for 20 ns;
+        wait;
+    end process;
+end;
